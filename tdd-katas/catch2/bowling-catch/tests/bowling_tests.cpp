ace5392b3fa2023fc8b9cca370f4a445e6de0caf
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-10-08 12:19:05 +0200

    Bowling - Test #4

--- tdd-katas/catch2/bowling-catch/tests/bowling_tests.cpp
+++ tdd-katas/catch2/bowling-catch/tests/bowling_tests.cpp
@@ -1,22 +1,52 @@
 #include <catch2/catch_test_macros.hpp>
 #include "bowling.hpp"
+#include <array>
 
 using namespace std;
 
 class BowlingGame
 {
+    static constexpr size_t max_pins_in_frame = 10;
+
 private:
-    int score_val = 0;
+    std::array<unsigned int, 20> pins_{};
+    
+    size_t roll_index_{};
+
+    size_t spare_bonus(size_t roll_index) const
+    {
+        return pins_[roll_index + 2];
+    }
+
+    size_t frame_score(size_t roll_index) const
+    {
+        return pins_[roll_index] + pins_[roll_index+1];
+    }
+
+    bool is_spare(size_t roll_index) const
+    {
+        return frame_score(roll_index) == max_pins_in_frame;
+    }
 
 public:
     int score() const
     {
-        return score_val;
+        size_t result{};
+
+        for(size_t i = 0; i < 20; i += 2)
+        {
+            if (is_spare(i))
+                result += spare_bonus(i); 
+            result += frame_score(i) ;
+        }
+
+        return result;
     }
 
     void roll(int pins)
     {
-        score_val += pins;
+        pins_[roll_index_] = pins;
+        ++roll_index_;
     }
 };
 
@@ -42,11 +72,23 @@ TEST_CASE("When all rolls are zero score is zero")
     REQUIRE(game.score() == 0);
 }
 
-TEST_CASE("When rolls are not zero score is valid")
+TEST_CASE("When rolls are not zero score is sum of pins")
 {
     BowlingGame game;
 
     roll_many(game, 20, 1);
 
     REQUIRE(game.score() == 20);
+}
+
+TEST_CASE("When spare next roll is counted twice")
+{
+    BowlingGame game;
+
+    game.roll(5);
+    game.roll(5);
+
+    roll_many(game, 18, 1);
+
+    REQUIRE(game.score() == 29);
 }
