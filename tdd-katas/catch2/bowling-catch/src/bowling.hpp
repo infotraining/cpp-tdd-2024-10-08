53590e0d0400acc2e4f79b3d8a8eb0ef089c18c4
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-10-08 14:17:37 +0200

    Move BowlingGame to hpp file

--- tdd-katas/catch2/bowling-catch/src/bowling.hpp
+++ tdd-katas/catch2/bowling-catch/src/bowling.hpp
@@ -1,14 +1,79 @@
 #ifndef BOWLING_HPP
 #define BOWLING_HPP
 
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <string>
+#include <vector>
+#include <array>
 
 namespace TDD
 {
-    class BowlingGame   
-    {};
+    class BowlingGame
+    {
+        static constexpr size_t max_pins_in_frame = 10;
+        static constexpr size_t frames_count = 10;
+
+    private:
+        std::array<unsigned int, 21> pins_{};
+
+        size_t roll_index_{};
+
+        size_t frame_score(size_t roll_index) const
+        {
+            return pins_[roll_index] + pins_[roll_index + 1];
+        }
+
+        bool is_spare(size_t roll_index) const
+        {
+            return frame_score(roll_index) == max_pins_in_frame;
+        }
+
+        size_t spare_bonus(size_t roll_index) const
+        {
+            return pins_[roll_index + 2];
+        }
+
+        bool is_strike(size_t roll_index) const
+        {
+            return pins_[roll_index] == max_pins_in_frame;
+        }
+
+        size_t strike_bonus(size_t roll_index) const
+        {
+            return pins_[roll_index + 1] + pins_[roll_index + 2];
+        }
+
+    public:
+        int score() const
+        {
+            size_t result{};
+            size_t roll_index{};
+
+            for (size_t frame_index = 0; frame_index < frames_count; ++frame_index)
+            {
+                if (is_strike(roll_index))
+                {
+                    result += max_pins_in_frame + strike_bonus(roll_index);
+                    ++roll_index;
+                }
+                else
+                {
+                    result += frame_score(roll_index);
+                    if (is_spare(roll_index))
+                        result += spare_bonus(roll_index);
+                    roll_index += 2;
+                }
+            }
+
+            return result;
+        }
+
+        void roll(int pins)
+        {
+            pins_[roll_index_] = pins;
+            ++roll_index_;
+        }
+    };
 }
 
 #endif
