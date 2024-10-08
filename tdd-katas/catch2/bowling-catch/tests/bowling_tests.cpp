8187b1d4c40cb477726c5642b77e3db1d8031239
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-10-08 11:36:12 +0200

    Bowling - Test #2

--- tdd-katas/catch2/bowling-catch/tests/bowling_tests.cpp
+++ tdd-katas/catch2/bowling-catch/tests/bowling_tests.cpp
@@ -5,10 +5,18 @@ using namespace std;
 
 class BowlingGame
 {
+private:
+    //int score_val = 0;
+
 public:
     int score() const
     {
         return 0;
+    }
+
+    void roll(int val)
+    {
+        //score_val += val;
     }
 };
 
@@ -19,3 +27,12 @@ TEST_CASE("When game starts score is zero")
     REQUIRE(game.score() == 0);
 }
 
+TEST_CASE("When all rolls are zero score is zero")
+{
+    BowlingGame game;
+
+    for(int i = 0; i < 20; ++i)
+        game.roll(0);
+
+    REQUIRE(game.score() == 0);
+}
