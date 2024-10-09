7fe34504df7d9fdb4a6dc9493191e0988e051d4b
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-10-09 15:27:51 +0200

    Snake - Test#1

--- snake/snake_tests/snake_tests.cpp
+++ snake/snake_tests/snake_tests.cpp
@@ -9,6 +9,64 @@
 
 using namespace std;
 
-TEST_CASE("Start the game")
+struct Point {
+    Point(int x , int y) : x{x}, y{y}
+    {}
+    
+    int x, y;    
+
+    bool operator==(const Point& other) const
+    {
+        return x == other.x && y == other.y;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Point& pt)
+    {
+        out << "Point{" << pt.x << ", " << pt.y << "}";
+        return out;
+    }
+};
+
+class Snake
 {
+private:
+    Point head_;
+public:
+    Snake(Point);
+    ~Snake();
+
+    Point head() {
+        return head_;
+    }
+
+    size_t segments() const
+    {
+        return 1;
+    }    
+};
+
+Snake::Snake(Point head) : head_{head}
+{
+}
+
+Snake::~Snake()
+{
+}
+
+TEST_CASE("Snake")
+{
+    SECTION("constructed with Point")
+    {
+        Snake snake(Point(10, 5));
+
+        SECTION("has head at specified location")
+        {
+            REQUIRE(snake.head() == Point(10, 5));
+        }
+
+        SECTION("has one segment")
+        {
+            REQUIRE(snake.segments() == 1);
+        }
+    }
 }
